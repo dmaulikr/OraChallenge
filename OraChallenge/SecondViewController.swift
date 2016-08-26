cba4e71cad73bbd59223f2df1d7a15cc4cf71d13
DanAlkemy <Dan@Alkemy.io>
2016-08-25 22:37:41 -0500

    Initial Commit

---
+++ OraChallenge/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OraChallenge
+//
+//  Created by Daniel Hahm on 8/25/16.
+//  Copyright © 2016 DanH. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
